b99ee16f0c54da5acc0c246bc02e2d4abecd97bb
Daksh Tandel <167323670+dakshtandel-9@users.noreply.github.com>
2024-10-02 21:37:27 +0530

    final commit 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import HomePage from './Home/HomePage';
-// import Navbar from './Navbar';
 import Footer from './Footer';
 
 // Render the application
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    {/* <Navbar /> */}
     <Routes>
       <Route path="/" element={<HomePage />} />
     </Routes>
